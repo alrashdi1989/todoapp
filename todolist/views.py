9c015afed428f9248572dd9f5a488b08636b708c
AbdulRahman Alrashdi <ALrashdi1989@gmail.com>
2021-06-02 05:55:13 +0400

    python web app

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,13 @@
+from todolist.models import Todolist
 from django.shortcuts import render
-
+from .models import Todolist
 # Create your views here.
 def index(requset):
-    return render(requset,'todolist\index.html')
+    todo_items = Todolist.objects.order_by('id') 
+    context ={'todo_items' : todo_items}
+    return render(requset,'todolist\index.html',context)
+
+def home(requset):
+    todo_items = Todolist.objects.order_by('id') 
+    context ={'todo_items' : todo_items}
+    return render(requset,'todolist\Home.html',context)
