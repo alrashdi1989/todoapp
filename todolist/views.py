d34e10ab75d2ff890d7235b209e3c7c8f052174c
alrashdi1989 <46470876+alrashdi1989@users.noreply.github.com>
2021-06-08 05:50:37 +0400

    new project

--- todolist/views.py
+++ todolist/views.py
@@ -1,13 +1,45 @@
-from todolist.models import Todolist
-from django.shortcuts import render
-from .models import Todolist
-# Create your views here.
-def index(requset):
-    todo_items = Todolist.objects.order_by('id') 
-    context ={'todo_items' : todo_items}
-    return render(requset,'todolist\index.html',context)
+from django.shortcuts import render, redirect
 
-def home(requset):
-    todo_items = Todolist.objects.order_by('id') 
-    context ={'todo_items' : todo_items}
-    return render(requset,'todolist\Home.html',context)
+from .models import Todolist
+
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
+
+# Create your views here.
+
+def index(request):
+    todo_items = Todolist.objects.order_by('id')
+    form = TodoListForm()
+    context = {'todo_items' :todo_items, 'form' : form}
+    return render(request,'todolist/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+    
+
+    return redirect('index')
+
+
+def completedTodo(request, todo_id)  :  
+    todo = Todolist.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
+
+    return redirect('index')
+
+
+def deleteCompleted(request):
+    Todolist.objects.filter(completed__exact=True).delete()
+
+    return redirect('index')
+
+def deleteAll(request):
+    Todolist.objects.all().delete()
+
+    return redirect('index')
